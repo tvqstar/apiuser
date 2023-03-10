49f1b94ed2afc32d590b454e50c7c9ae84edb3be
TVQ <tavanquyet2206@gmail.com>
2023-03-10 10:33:16 +0700

    a

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const cors = require('cors');
 
 const port = 5000;
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
